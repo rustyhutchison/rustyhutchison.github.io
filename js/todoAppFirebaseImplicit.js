6a09d5c99d0342d20ffb88d4b0c75850cfa527a3
Rusty Hutchison <rustywill7778@gmail.com>
2016-09-08 23:46:06 -0400

    go

--- js/todoAppFirebaseImplicit.js
+++ js/todoAppFirebaseImplicit.js
@@ -8,9 +8,9 @@ var TodoList3 = React.createClass({
       return (
         <li key={ index }>
           <div>key: { item['.key'] }</div>
-          <div>first name: { item.name}</div>
-          <div>position: { item.position }</div>
-          <div>height: { item.height }</div>
+          <div>first name: { item.category}</div>
+          <div>position: { item.price }</div>
+          <div>height: { item.name }</div>
           <div><span onClick={ _this.props.removeItem.bind(null, item['.key']) }
                 style={{ color: 'red', marginLeft: '10px', cursor: 'pointer' }}>
                   Delete Player
@@ -29,9 +29,10 @@ var TodoApp3 = React.createClass({
   getInitialState: function() {
     return {
       items: [],
-      name: '',
-      position: '',
-      height: '',
+      category: '',
+      price: '',
+      stocked: '',
+      name: ''
     };
   },
 
@@ -56,19 +57,19 @@ var TodoApp3 = React.createClass({
     e.preventDefault();
     if (this.state.name && this.state.name.trim().length !== 0) {
       this.firebaseRefs['products'].push({
-        name: this.state.name,
-        position: this.state.position, // "name:" changes the input attribute category
-        height: this.state.height
+        category: this.state.category,
+        price: this.state.price, // "name:" changes the input attribute category
+        name: this.state.name
       });
       this.setState({
-        name: '',
-        position: '',
-        height: ''
+        category: '',
+      	price: '',
+      	name: ''
       });
     }
+    this.state.category = String.Empty;
+    this.state.price = String.Empty;
     this.state.name = String.Empty;
-    this.state.position = String.Empty;
-    this.state.height = String.Empty;
   },
 
   render: function() {
@@ -76,9 +77,9 @@ var TodoApp3 = React.createClass({
       <div>
         <TodoList3 prospects={ this.state.products } removeItem={ this.removeItem } />
         <form onSubmit={ this.handleSubmit }>
-          <div>Name: <input onChange={ this.onChange } value={ this.state.name } name="name"/></div>
-          <div>Position: <input onChange={ this.onChange } value={ this.state.position } name="position"/></div>
-          <div>Height: <input onChange={ this.onChange } value={ this.state.height } name="height"/></div>
+          <div>Name: <input onChange={ this.onChange } value={ this.state.category } name="category"/></div>
+          <div>Position: <input onChange={ this.onChange } value={ this.state.price } name="price"/></div>
+          <div>Height: <input onChange={ this.onChange } value={ this.state.name } name="name"/></div>
           <button>{ 'Add #' + (this.state.items.length + 1) }</button>
         </form>
       </div>
