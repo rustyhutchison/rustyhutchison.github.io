8d9602bddd88c719938cfb80f15946dd6d107eec
Rusty Hutchison <rustywill7778@gmail.com>
2016-09-09 13:32:13 -0400

    go

--- js/todoAppFirebaseImplicit.js
+++ js/todoAppFirebaseImplicit.js
@@ -1,6 +1,40 @@
 
 
 
+var SearchBar = React.createClass({
+    
+    handleChange: function() {
+        this.props.onUserInput(
+            this.refs.filterTextInput.value,
+            this.refs.inStockOnlyInput.checked
+        );
+    },
+    
+    render: function() {
+        return (
+            <form>
+                <input 
+                	type="text"
+                    placeholder="Search..."
+                    value={this.props.filterText}
+                    ref="filterTextInput"
+                    onChange={this.handleChange}
+                 />
+                <p>
+                    <input 
+                    	type="checkbox"
+                        checked={this.props.inStockOnly}
+                        ref="inStockOnlyInput"
+                        onChange={this.handleChange}
+                    />
+                    {' '}
+                    Only show products in stock
+                </p>
+            </form>
+        );
+    }
+});
+
 var TodoList3 = React.createClass({
   render: function() {
     var _this = this;
