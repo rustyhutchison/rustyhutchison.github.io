acc962eefd379acd9b305b6922710b2b00e60635
Rusty Hutchison <rustywill7778@gmail.com>
2016-09-12 02:08:44 -0400

    go

--- js/todoAppFirebaseImplicit.js
+++ js/todoAppFirebaseImplicit.js
@@ -55,7 +55,7 @@ var ProductInput = React.createClass({
         <form onSubmit={ this.handleSubmit }>
 		  <div className="">
 				<label >Category</label>
-				<select onChange={ this.onChange } name="category" className="form-control standalone" type="select" label="Select" placeholder="select">
+				<select value="" onChange={ this.onChange } name="category" className="form-control standalone" type="select" label="Select" placeholder="select">
 		   		    <option value="" defaultValue>Select</option>
 			        <option value="Sporting Goods">Sporting Goods</option>
 					<option value="Electronics">Electronics</option>
@@ -66,7 +66,7 @@ var ProductInput = React.createClass({
 		  <div>Price: <input onChange={ this.onChange } value={ this.state.price } name="price"/></div>
 		  <div className="">
 				<label >In Stock?</label>
-				<select onChange={ this.onChange } name="stocked" className="form-control standalone" type="select" label="Select" placeholder="select">
+				<select value="" onChange={ this.onChange } name="stocked" className="form-control standalone" type="select" label="Select" placeholder="select">
 					<option value="" defaultValue>Select</option>
 					<option value="True">True</option>
 					<option value="False">False</option>
