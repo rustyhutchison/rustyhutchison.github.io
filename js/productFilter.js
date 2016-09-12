fed8636845afd5a20c5c700271f1c6c8f2302bdf
Rusty Hutchison <rustywill7778@gmail.com>
2016-09-12 03:35:23 -0400

    go

--- js/productFilter.js
+++ js/productFilter.js
@@ -32,6 +32,7 @@ var ProductTable = React.createClass({
     
     render: function() {
         var rows = [];
+        var newRows =[];
         var lastCategory = null;
         this.props.products.forEach(function(product) {
             
@@ -41,6 +42,13 @@ var ProductTable = React.createClass({
             
             
             rows.push(<ProductRow product={product} key={product.name} removeItem={this.props.removeItem } />);
+            var newRows = function() {
+            	rows.sort(function(a, b){
+    			if(a.name < b.name) return -1;
+    			if(a.name > b.name) return 1;
+    			return 0;
+				})
+			}
             lastCategory = product.category;
         }.bind(this));
         return (
@@ -52,7 +60,7 @@ var ProductTable = React.createClass({
                         <th>Category</th>
                     </tr>
                 </thead>
-                <tbody>{rows}</tbody>
+                <tbody>{newRows}</tbody>
             </table>
         );
     }
