c33a77ab0dab205ca6dc1059a31a25e250a61ec5
Rusty Hutchison <rustywill7778@gmail.com>
2016-09-12 02:00:24 -0400

    go

--- js/playerFilter.js
+++ js/playerFilter.js
@@ -51,7 +51,6 @@ var ProductTable = React.createClass({
                         <th>Name</th>
                         <th id="price">Price</th>
                     </tr>
-                    <hr>
                 </thead>
                 <tbody>{rows}</tbody>
             </table>
