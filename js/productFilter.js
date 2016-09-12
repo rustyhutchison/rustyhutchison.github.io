d881a5cbbc69bd3adbbef17897de4a56c9b22e6f
Rusty Hutchison <rustywill7778@gmail.com>
2016-09-12 02:39:41 -0400

    go

--- js/productFilter.js
+++ js/productFilter.js
@@ -42,7 +42,11 @@ var ProductTable = React.createClass({
                 rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
             }
             rows.push(<ProductRow product={product} key={product.name} removeItem={this.props.removeItem } />);
-            rows.category.sort();
+            rows.sort(function(a, b) {
+    			var textA = a.category.toUpperCase();
+    			var textB = b.category.toUpperCase();
+    			return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+			});
             lastCategory = product.category;
         }.bind(this));
         return (
