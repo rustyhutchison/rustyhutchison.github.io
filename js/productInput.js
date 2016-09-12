21a2667e65a9cd99fee5a897ad621b8a46801cbd
Rusty Hutchison <rustywill7778@gmail.com>
2016-09-12 13:10:54 -0400

    go

--- js/productInput.js
+++ js/productInput.js
@@ -52,7 +52,7 @@ var ProductInput = React.createClass({
 
   render: function() {
     return (
-      <div id="input-container" className="text-left">
+      <div className="text-left">
       	<h2>Input Products</h2>
         <form onSubmit={ this.handleSubmit }>
 		  <div className="">
