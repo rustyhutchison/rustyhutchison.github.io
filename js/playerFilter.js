066e65729f485cfeb500d89fbc184cdbdb4a75a5
Rusty Hutchison <rustywill7778@gmail.com>
2016-09-11 21:27:24 -0400

    go

--- js/playerFilter.js
+++ js/playerFilter.js
@@ -32,9 +32,9 @@ var ProductTable = React.createClass({
             }
             
             if (product.category !== lastCategory) {
-                rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+                rows.push(<ProductCategoryRow category={product.category} key={product[0]} />);
             }
-            rows.push(<ProductRow product={product} key={product.name} />);
+            rows.push(<ProductRow product={product} key={product[0]} />);
             lastCategory = product.category;
         }.bind(this));
         return (
